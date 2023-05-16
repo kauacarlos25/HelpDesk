174df75486428c96ff23c495613021df66a7f8f4
Kaua <letesk@gmail.com>
2023-05-16 11:17:48 -0300

    Manutenção do Sistema

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn_imp = document.getElementById('btn_imp')
+
+btn_imp.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+
+    const btn_imp = document.querySelector('#btn_imp')
+    const value = btn.value;
+})
